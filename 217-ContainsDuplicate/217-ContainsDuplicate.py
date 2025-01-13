85f2cc84d4faf2b03784f98a452666327280d552
roburnto <58539522+roburnto@users.noreply.github.com>
2025-01-12 20:17:33 -0500

    Create file

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -0,0 +1,9 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash = set()
+        for num in nums:
+            if num not in hash:
+                hash.add(num)
+            else:
+                return True
+        return False
