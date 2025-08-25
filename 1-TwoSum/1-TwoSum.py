00ebee79e7d2375ba67d303cf2ccfcc2f857d1c9
Roberto Rodriguez <58539522+roburnto@users.noreply.github.com>
2025-08-25 10:49:01 -0400

    File updated at 8/25/2025, 10:49:01 AM

--- 1-TwoSum/1-TwoSum.py
+++ 1-TwoSum/1-TwoSum.py
@@ -1,12 +1,10 @@
+# Last updated: 8/25/2025, 10:49:01 AM
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         index = {}
 
         for i in range(len(nums)):
-            complement = target - nums[i]
-            
+            complement = target-nums[i]
             if complement in index:
-                return [i,index[complement]]
-            
+                return [index[complement], i]
             index[nums[i]] = i
-
