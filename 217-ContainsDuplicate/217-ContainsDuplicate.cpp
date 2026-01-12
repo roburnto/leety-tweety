88c8a49a4a35280253490d76413657f82aba35cc
Roberto Rodriguez <58539522+roburnto@users.noreply.github.com>
2026-01-12 13:02:55 -0500

    File updated at 1/12/2026, 1:02:53 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.cpp
+++ 217-ContainsDuplicate/217-ContainsDuplicate.cpp
@@ -1,14 +1,15 @@
-// Last updated: 1/12/2026, 1:00:34 PM
+// Last updated: 1/12/2026, 1:02:53 PM
 1class Solution {
 2public:
 3    bool containsDuplicate(vector<int>& nums) {
-4        std::set<int> set{};
-5        for (int num: nums){
-6            if (set.contains(num)){
-7                return true;
-8            }
-9            set.insert(num);
-10        }
-11        return false;
-12    }
-13};
+4        // use std::unordered_set<int>
+5        std::unordered_set<int> seen;
+6        for (int num: nums){
+7            if (seen.count(num)){
+8                return true;
+9            }
+10            seen.insert(num);
+11        }
+12        return false;
+13    }
+14};
