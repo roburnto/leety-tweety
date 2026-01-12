e068e542986dbdde87816cce9292107e94c7605d
Roberto Rodriguez <58539522+roburnto@users.noreply.github.com>
2026-01-12 13:41:37 -0500

    Create 1019-SquaresOfASortedArray

---
+++ 1019-SquaresOfASortedArray/1019-SquaresOfASortedArray.py
@@ -0,0 +1,16 @@
+# Last updated: 1/12/2026, 1:41:35 PM
+class Solution:
+    def sortedSquares(self, nums):
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) < abs(nums[right]):
+                square = nums[right]
+                right -= 1
+            else:
+                square = nums[left]
+                left += 1
+            result[i] = square * square
+        return result
