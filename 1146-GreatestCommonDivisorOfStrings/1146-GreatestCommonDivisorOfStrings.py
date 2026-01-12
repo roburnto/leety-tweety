9360470c3d2d87d600b3bdcdf69a674bafcce524
Roberto Rodriguez <58539522+roburnto@users.noreply.github.com>
2026-01-12 13:41:33 -0500

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.py
@@ -0,0 +1,10 @@
+# Last updated: 1/12/2026, 1:41:31 PM
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        gcd_length = gcd(len(str1),len(str2))
+        
+        return str1[:gcd_length]
